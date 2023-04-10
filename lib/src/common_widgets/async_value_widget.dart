15390cb43970d550f7dd17da816d2fc2cdba705d
Marcin Krasowski <baaltip@gmail.com>
2023-04-10 11:09:25 +0200

    Initial commit

---
+++ lib/src/common_widgets/async_value_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AsyncValueWidget<T> extends StatelessWidget {
+  const AsyncValueWidget({super.key, required this.value, required this.data});
+  final AsyncValue<T> value;
+  final Widget Function(T) data;
+
+  @override
+  Widget build(BuildContext context) {
+    return value.when(
+      data: data,
+      error: (e, st) => Center(
+        child: Text('error: $e with st: $st'),
+      ),
+      loading: () => const Center(child: CircularProgressIndicator()),
+    );
+  }
+}
+
+/// Sliver equivalent of [AsyncValueWidget]
+class AsyncValueSliverWidget<T> extends StatelessWidget {
+  const AsyncValueSliverWidget(
+      {super.key, required this.value, required this.data});
+  final AsyncValue<T> value;
+  final Widget Function(T) data;
+
+  @override
+  Widget build(BuildContext context) {
+    return value.when(
+      data: data,
+      loading: () => const SliverToBoxAdapter(
+          child: Center(child: CircularProgressIndicator())),
+      error: (e, st) => SliverToBoxAdapter(
+        child: Center(
+          child: Text(
+            'error: $e with st: $st',
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/localization/string_hardcoded.dart
@@ -0,0 +1,5 @@
+extension StringHardcoded on String {
+  // use this annotation to highlight all of the hardcoded strings
+  // @Deprecated("TODO: Add localization")
+  String get hardcoded => this;
+}
